3573089c485282e8a1d716567ce8101cffe3eca8
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-06-05 15:18:45 +0600

    updated(): api for kyc status change

---
+++ app/graphql/mutations/update_kyc_status.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UpdateKycStatus < BaseMutation
+    class UpdateKycStatusAttributes < Types::BaseInputObject
+      argument :id, ID, required: true
+      argument :kyc_status, Types::KycStatusEnum, required: true
+    end
+
+    argument :attributes, UpdateKycStatusAttributes, required: true
+    type Types::ProfileType
+
+    def resolve(attributes:)
+      Profile.find(attributes.to_h[:id]).update!(kyc_status: attributes.to_h[:kyc_status])
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :send_for_kyc, mutation: Mutations::SendForKyc
     field :create_certificate, mutation: Mutations::CreateCertificate
     field :generate_certificate_pdf, mutation: Mutations::GenerateCertificatePdf
+    field :update_kyc_status, mutation: Mutations::UpdateKycStatus
   end
 end
