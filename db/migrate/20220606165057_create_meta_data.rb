8d552b37b9ec761b48f28538a720756efa943c33
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-06-06 22:59:06 +0600

    feat(): added meta datum

---
+++ db/migrate/20220606165057_create_meta_data.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateMetaData < ActiveRecord::Migration[7.0]
+  def change
+    create_table :meta_data do |t|
+      t.string :image_url, null: false
+      t.string :description, null: false
+      t.string :name, null: false
+      t.jsonb :traits, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_605_133_127) do
+ActiveRecord::Schema[7.0].define(version: 20_220_606_165_057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -57,6 +57,15 @@ ActiveRecord::Schema[7.0].define(version: 20_220_605_133_127) do
     t.datetime 'year_awarded'
     t.string 'qr_code_url'
     t.index ['user_id'], name: 'index_certificates_on_user_id'
+  end
+
+  create_table 'meta_data', force: :cascade do |t|
+    t.string 'image_url', null: false
+    t.string 'description', null: false
+    t.string 'name', null: false
+    t.jsonb 'traits', default: [], array: true
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
   create_table 'profiles', force: :cascade do |t|
